c13398f445e379c813c234e7d279eadaad2a52a9
AIntosha <56848124+AIntosha@users.noreply.github.com>
2020-08-12 10:25:03 +0300

    Update main.py

--- Pomodoro_timer/main.py
+++ Pomodoro_timer/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 import os
 
 # base states
@@ -32,7 +33,7 @@ def update_break():
                 break_count.after(1000, update_break)
                 counter_break -= 1
             else:
-                playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'endbrake.mp3'))
+                play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'endbrake.mp3')) - 20)
                 counter_timer = 1500
                 counter_break = 300
                 timer_count.configure(text='25:00')
@@ -74,7 +75,7 @@ def update_timer():
                 counter_timer -= 1
             else:
                 if alreadyplayed == 0:
-                    playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'victory.mp3'))
+                    play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'victory.mp3')) - 20)
                     alreadyplayed = 1
                     rdy += 1
                     rdy_count.configure(text=rdy)
@@ -99,7 +100,7 @@ def click_start():
     stop.grid(column=2, row=5)
     start.grid_forget()
     update_timer()
-    playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'start.mp3'))
+    play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'start.mp3')) - 20)
 
 
 def click_pause():
@@ -109,14 +110,12 @@ def click_pause():
         pause.configure(text='Resume')
         paused = 1
         update_timer()
-        playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'pause.mp3'))
-        # playsound('sounds/pause.mp3')
+        play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'pause.mp3')) - 20)
     else:
         pause.configure(text='Pause')
         paused = 0
         update_timer()
-        playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'resume.mp3'))
-        # playsound('sounds/resume.mp3')
+        play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'resume.mp3')))
 
 
 def click_stop():
@@ -135,35 +134,35 @@ def click_stop():
     stop.grid_forget()
     pause.grid_forget()
     start.grid(column=0, row=5)
-    playsound(os.path.join(os.path.dirname(__file__), 'sounds', 'stop.mp3'))
+    play(AudioSegment.from_file(os.path.join(os.path.dirname(__file__), 'sounds', 'stop.mp3')) - 20)
 
 
 # window properties
 window = Tk()
-window.title("Pomodoro")
-window.geometry('300x150')
+window.title("Timer")
+window.geometry('390x150')
 
 # main timer
-timer_text = Label(window, text="timer", font=("Helvetica"))
+timer_text = Label(window, text="timer", font=("Times", "20", "bold"))
 timer_text.grid(column=0, row=0, padx=10, pady=10)
-timer_count = Label(window, text="25:00", font=("Helvetica"))
+timer_count = Label(window, text="25:00", font=("Times", "20", "bold"))
 timer_count.grid(column=1, row=0, padx=10, pady=10)
 
 # break timer
-break_text = Label(window, text="break", font=("Helvetica"))
+break_text = Label(window, text="break", font=("Times", "20", "bold"))
 break_text.grid(column=0, row=1, padx=10, pady=10)
-break_count = Label(window, text="5:00", font=("Helvetica"))
+break_count = Label(window, text="5:00", font=("Times", "20", "bold"))
 break_count.grid(column=1, row=1, padx=10, pady=10)
 
 # count of ready
-rdy_text = Label(window, text='Num of Ready', font=("Helvetica"))
+rdy_text = Label(window, text='Num of Ready', font=("Times", "20", "bold"))
 rdy_text.grid(column=2, row=0)
-rdy_count = Label(window, text='0', background='orange', font=("Helvetica"))
+rdy_count = Label(window, text='0', background='orange', font=("Times", "20", "bold"))
 rdy_count.grid(column=2, row=1)
 
-start = Button(window, text="Start", command=click_start, background='orange', font=("Helvetica"))
-pause = Button(window, text="Pause", command=click_pause, font=("Helvetica"))
-stop = Button(window, text="Stop", command=click_stop, font=("Helvetica"))
+start = Button(window, text="Start", command=click_start, background='orange', font=("Times", "20", "bold"))
+pause = Button(window, text="Pause", command=click_pause, font=("Times", "20", "bold"))
+stop = Button(window, text="Stop", command=click_stop, font=("Times", "20", "bold"))
 start.grid(column=0, row=5)
 
 window.mainloop()
