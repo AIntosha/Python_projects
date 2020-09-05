d2f8a677e997cd43246e5b720c19f85294b6d61c
AIntosha <56848124+AIntosha@users.noreply.github.com>
2020-09-05 15:39:49 +0300

    Add files via upload

---
+++ hh_parser_api/parser_hh_api_v1.0.py
@@ -0,0 +1,58 @@
+import requests
+import statistics
+
+vacancy = input('Введите название вакансии и нажмите Enter:\n')
+print('Это займет не больше минуты')
+
+usdrate = 80
+vacancywithSalary = 0
+avgsalarylist = list()
+
+
+def responce(page, per_page, text=vacancy):
+    responce = requests.get('https://api.hh.ru/vacancies/',
+                            headers={'User-Agent': 'Python (fuoff@gmail.com)'},
+                            params={
+                                'text': text,
+                                'page': page,
+                                'per_page': per_page
+                            })
+    return responce.json()
+
+
+# вывести все вакансии в столбик
+for x in range(responce(page=0, per_page=100)['pages']):
+    templist = responce(page=x, per_page=100)['items']
+    for i in templist:
+        if i['salary'] is None:
+            continue
+        else:
+            vacancywithSalary += 1
+            if i['salary']['currency'] == 'RUR':
+                if i['salary']['from'] is None and i['salary']['to'] is not None:
+                    avgsalarylist.append(i['salary']['to'])
+                elif i['salary']['from'] is not None and i['salary']['to'] is None:
+                    avgsalarylist.append(i['salary']['from'])
+                elif i['salary']['from'] is not None and i['salary']['to'] is not None:
+                    avgsalarylist.append(int(round((i['salary']['from'] + i['salary']['to']) / 2)))
+                else:
+                    print('ERRRRRORRRR')
+            # USD
+            else:
+                if i['salary']['from'] is None and i['salary']['to'] is not None:
+                    avgsalarylist.append(i['salary']['to'] * usdrate)
+                elif i['salary']['from'] is not None and i['salary']['to'] is None:
+                    avgsalarylist.append(i['salary']['from'] * usdrate)
+                elif i['salary']['from'] is not None and i['salary']['to'] is not None:
+                    avgsalarylist.append(int(round(((i['salary']['from'] + i['salary']['to']) / 2) * usdrate)))
+                else:
+                    print('ERRRRRORRRR')
+
+print('Профессия:', vacancy)
+print('Всего вакансий:', responce(page=0, per_page=1)['pages'])
+print('Вакансий с ценой:', vacancywithSalary)
+print('Средняя зарплата:', int(round(statistics.median(avgsalarylist))), 'руб')
+
+print('---------------')
+print('Нажми Enter чтобы выйти')
+input()
